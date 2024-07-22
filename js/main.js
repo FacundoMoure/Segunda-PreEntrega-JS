ef95f164baea97b7b6f229a9853fe001e2d91ce8
Facundo Moure <facundo.moure@gmail.com>
2024-07-22 20:41:04 -0300

    se agregan validaciones

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 
 while (true) {
-    const nombreProductoBuscado = prompt("¿Desea consultar sobre una guitarra, un bajo o precio de una calibración? \nsino, escriba 's' para salir:");
+    const nombreProductoBuscado = prompt("¿Desea consultar sobre una guitarra, un bajo o precio de una calibración?\nEscriba 'guitarra', 'bajo', 'calibracion' o 's' para salir:");
 
     if (nombreProductoBuscado.toLowerCase() === "s") {
-        break; 
+        break;
+    }
+
+    if (nombreProductoBuscado.toLowerCase() !== "guitarra" && nombreProductoBuscado.toLowerCase() !== "bajo" && nombreProductoBuscado.toLowerCase() !== "calibracion") {
+        console.log("Por favor, ingrese 'guitarra', 'bajo', 'calibracion' o 's' para salir.");
+        continue; 
     }
 
     const resultadosBusqueda = buscarProductoPorNombre(nombreProductoBuscado);
